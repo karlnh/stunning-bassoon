ab3fb2d14aa21a7b9fe8a6892eaf83e45b0457a6
karlnh <hubigkarl@gmail.com>
2023-02-22 23:12:45 -0600

    add home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
