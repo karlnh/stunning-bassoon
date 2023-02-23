4f05cf526898f4dbe7850f5993090e114799cb56
karlnh <hubigkarl@gmail.com>
2023-02-23 00:07:32 -0600

    add seed data

---
+++ seeds/seed.js
@@ -0,0 +1,41 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+let posts = [];
+let comments = [];
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    const newPost = await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+    posts.push(newPost);
+  };
+
+  for (const comment of commentData) {
+    const newComment = await Comment.create({
+        ...comment,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    });
+    comments.push(newComment);
+  };
+
+  console.log(comments);
+
+  process.exit(0);
+};
+
+seedDatabase();
