7d9ab2c0de70bcf8f3d811b7eacd035a8b273869
karlnh <hubigkarl@gmail.com>
2023-02-24 00:44:22 -0600

    add login and logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,8 +12,47 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData);
         });
     } catch (err) {
-        res.status(400).json(err);
+        console.error(err);
+        res.status(500).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: { username: req.body.username}
+        });
+        const validPass = userData.checkPassword(req.body.password);
+        if (!userData || !validPass) {
+            res
+                .status(400)
+                .json({ message: 'Error logging in.'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.logged_in = true;
+
+            res
+                .status(200)
+                .json({ message: "Successfully logged in."});
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+})
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
